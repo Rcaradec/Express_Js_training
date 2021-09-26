8eabfd0ed662f93d5138386fd63ae23725731c41
Rcaradec <rcaradec.dev@gmail.com>
2021-09-26 18:21:54 +0200

    init ejs / ville.ejs

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 // on récupère le module dayjs pour gérer les dates et heures
 const dayjs = require("dayjs");
+// on switch dayjs en Francais
+require("dayjs/locale/fr");
+dayjs.locale("fr"); // use locale globally
 
 // on installe aussi son plugin pour gérer les TZ
 const utc = require("dayjs/plugin/utc");
@@ -21,6 +24,13 @@ const app = express();
 // on recupere notre module Weather
 var weather = require("weather-js");
 const { log } = require("console");
+const { response } = require("express");
+
+// Voici ou seront rangé les vues/templates
+app.set("views", "./views");
+
+// Je configure express pour lui préciser quel moteur de template on utilise
+app.set("view engine", "ejs");
 
 // On indique à Express que les ressources "statiques" (css, images, polices, etc.) sont rangées dans le dossiers 'public'
 app.use(express.static("public"));
@@ -49,7 +59,9 @@ app.get("/city/:ville", (req, res) => {
 
   // Récupérer le contenu du champs `tz` de la bonne entrée
   const dateFormat = dayjs().tz(infoVille.tz).format("DD MMMM YYYY HH:mm");
-  res.send(`Coucou, la ville est ${infoVille.name} , date : ${dateFormat}`);
+  // res.send(`Coucou, la ville est ${infoVille.name} , date : ${dateFormat}`);
+  // On demande à express de faire passer EJS sur le fichier ville.ejs
+  res.render("ville");
 });
 
 // On demande à express d'écouter les requetes sur le port stocké dans la variable `port`
